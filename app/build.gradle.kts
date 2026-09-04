46ee45049650889fd63fcb81d1258a417830588b
Tobias <73227066+PaperTobi@users.noreply.github.com>
2026-09-04 00:29:09 +0000

    Bump versionCode to 3 and versionName to 0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "com.fivius.wallet"
         minSdk = 28
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
